4bcf3b3258b3520955d21732bd8488deb7673fbe
Andy Wright <atwright147@gmail.com>
2015-10-16 14:43:14 +0100

    split tests for each mode into individual specs

---
+++ test/modeFirstSpec.js
@@ -0,0 +1,55 @@
+describe('Accordion directive in mode "first"', function() {
+
+	var dl, dt, dd;
+
+	beforeEach(function() {
+		browser.get('http://localhost:8080/');
+	});
+
+	beforeEach(function() {
+		// make initial selections
+		dl = element(by.css('dl[mode="first"]'));
+		dt = element.all(by.css('dl[mode="first"] dt'));  // will generate an array, so need to use .first()
+		dd = element.all(by.css('dl[mode="first"] dd'));  // will generate an array, so need to use .first()
+	});
+
+	it('should initially set first <dt> & <dl> class to be in open state.', function() {		
+		// check title class
+		expect(dt.first().getAttribute('class')).not.toMatch('closed');
+		expect(dt.first().getAttribute('class')).toMatch('open');
+
+		// check description class
+		expect(dd.first().getAttribute('class')).not.toMatch('hidden');
+	});
+
+	it('should initially set second++ <dt>s & <dl>s class to be in closed state.', function() {		
+		/* second item */
+		// check title class
+		expect(dt.get(1).getAttribute('class')).not.toMatch('open');
+		expect(dt.get(1).getAttribute('class')).toMatch('closed');
+
+		// check description class
+		expect(dd.get(1).getAttribute('class')).toMatch('hidden');
+		
+		/* third item */
+		// check title class
+		expect(dt.get(2).getAttribute('class')).not.toMatch('open');
+		expect(dt.get(2).getAttribute('class')).toMatch('closed');
+
+		// check description class
+		expect(dd.get(2).getAttribute('class')).toMatch('hidden');
+	});
+
+	it('should change css classes of target <dt> and child <dd> (to "closed" / "hidden") when target <dt> is clicked.', function() {
+		// click the <dt> tag
+		dt.first().click();
+
+		// check title class
+		expect(dt.first().getAttribute('class')).not.toMatch('open');
+		expect(dt.first().getAttribute('class')).toMatch('closed');
+
+		// check description class
+		expect(dd.first().getAttribute('class')).toMatch('hidden');
+	});
+
+});
