1533b066fe76a985103b14240e7b858b3c5e2412
Andy Wright <atwright147@gmail.com>
2015-10-22 15:54:23 +0100

    require `html5mode` to allow proper hashes in url

--- app.js
+++ app.js
@@ -4,3 +4,10 @@
 * Description
 */
 angular.module('app', ['accordion']);
+
+angular.module('app').config(function($locationProvider) {
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+});
