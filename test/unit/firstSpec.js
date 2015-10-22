130e468df24ce23c98aca9d85781c437ca4bba78
Andy Wright <atwright147@gmail.com>
2015-10-22 15:11:16 +0100

    finish replicating protractor tests for `mode="first"`

--- test/unit/firstSpec.js
+++ test/unit/firstSpec.js
@@ -1,8 +1,9 @@
 (function() {
 	'use strict';
 
-	describe('Accordion with mode "first"', function () {
+	describe('Accordion with mode set to "first"', function () {
 		var el, $compile, $rootScope, $scope;
+		var dt, dd, clickEvent;
 
 		beforeEach(module('app'));
 
@@ -32,15 +33,20 @@
 			$scope.$digest();
 		}));
 
-// it('should initially set first <dt> & <dl> class to be in open state.', function() {		
-// it('should initially set second++ <dt>s & <dl>s class to be in closed state.', function() {		
-// it('should change css classes of target <dt> and child <dd> to inverse of current when target <dt> is clicked.', function() {
+		beforeEach(function() {
+			dt = el.find('dt');
+			dd = el.find('dd');
+
+			clickEvent = new MouseEvent('click', {
+				'view': window,
+				'bubbles': true,
+				'cancelable': false
+			});
+		});
 
 
-		it('should initialise with first item open and others closed', function () {
-			var dt = el.find('dt');
-			var dd = el.find('dd');
 
+		it('should initialise with first item open and others closed', function() {
 			// first item
 			expect(angular.element(dt[0]).hasClass('open')).toBe(true);
 			expect(angular.element(dt[0]).hasClass('closed')).toBe(false);
@@ -51,6 +57,36 @@
 			expect(angular.element(dd[1]).hasClass('hidden')).toBe(true);
 			expect(angular.element(dt[2]).hasClass('closed')).toBe(true);
 			expect(angular.element(dd[2]).hasClass('hidden')).toBe(true);
+		});
+
+		it('should close the first item when it\'s title is clicked', function() {
+			// check classes before clicking
+			expect(angular.element(dt[0]).hasClass('open')).toBe(true);
+			expect(angular.element(dt[0]).hasClass('closed')).toBe(false);
+			expect(angular.element(dd[0]).hasClass('hidden')).toBe(false);
+
+			// click on the first title
+			dt[0].dispatchEvent(clickEvent);
+
+			// check classes after clicking
+			expect(angular.element(dt[0]).hasClass('closed')).toBe(true);
+			expect(angular.element(dt[0]).hasClass('open')).toBe(false);
+			expect(angular.element(dd[0]).hasClass('hidden')).toBe(true);
+		});
+
+		it('should open an item when it\'s title is clicked', function() {
+			// check classes before clicking
+			expect(angular.element(dt[2]).hasClass('closed')).toBe(true);
+			expect(angular.element(dt[2]).hasClass('open')).toBe(false);
+			expect(angular.element(dd[2]).hasClass('hidden')).toBe(true);
+
+			// click on the first title
+			dt[2].dispatchEvent(clickEvent);
+
+			// check classes after clicking
+			expect(angular.element(dt[2]).hasClass('open')).toBe(true);
+			expect(angular.element(dt[2]).hasClass('closed')).toBe(false);
+			expect(angular.element(dd[2]).hasClass('hidden')).toBe(false);
 		});
 
 	});
